73a6461cfe2b8a95630ee85e6ab53b1db45bc3a0
Parvez Alam <alamparvez301@gmail.com>
2018-02-15 02:03:24 +0530

    Weather app in react

---
+++ README.md
@@ -0,0 +1,5 @@
+# Weather Web App In React
+```
+> npm install
+> npm start
+```
---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API_KEY = '136339f0432a2b996aac9cebb73835e0';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},in`;
+	const request = axios.get(url);
+
+	//console.log("request:", request );
+
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
+}
